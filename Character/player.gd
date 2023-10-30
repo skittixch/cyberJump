bae33f60a16445867417d255e0602f1b93091600
Eric Bacus <eric@ericbacus.com>
2023-10-30 12:07:39 -0500

    more tutorial following

--- Character/player.gd
+++ Character/player.gd
@@ -5,22 +5,30 @@ extends CharacterBody2D
 @export var jump_velocity : float = -200.0
 @export var double_jump_velocity : float = -100
 
+@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var has_double_jumped : bool = false
+var animation_locked : bool = false
+var direction : Vector2 = Vector2.ZERO
+var was_in_air : bool = false
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		was_in_air = true
 	else:
 		has_double_jumped = false
+		if was_in_air == true:
+			land()
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
 			#normal jump from floor
-			velocity.y = jump_velocity
+			jump()
 		elif not has_double_jumped:
 			# Double jump in air
 			velocity.y = double_jump_velocity
@@ -28,10 +36,35 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("left", "right")
+	direction = Input.get_vector("left", "right", "up", "down")
 	if direction:
-		velocity.x = direction * speed
+		velocity.x = direction.x * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
-
+	update_animation()
 	move_and_slide()
+	update_facing_direction()
+	
+func update_animation():
+	if not animation_locked:
+		if direction.x != 0:
+			animated_sprite.play("run")
+		else:
+			animated_sprite.play("idle")
+			
+func update_facing_direction():
+	if direction.x > 0:
+		animated_sprite.flip_h = false
+	if direction.x < 0:
+		animated_sprite.flip_h = true
+		
+func jump():
+	velocity.y= jump_velocity
+	animated_sprite.play("jump_start")
+	animation_locked = true
+	animated_sprite.play("jump_start")
+	animation_locked = true
+
+func land():
+	animated_sprite.play("jump_end")
+	animation_locked = true
